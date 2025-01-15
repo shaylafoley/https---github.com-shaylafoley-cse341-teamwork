1e5f246e0f484bda714210fb369476b35396dece
shaylafoley <wholemotherhood@gmail.com>
2025-01-15 14:43:44 -0500

    changes to assignment

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 app
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options))
   .use(cors())
   .use(express.json())
   .use(express.urlencoded({ extended: true }))
   .use('/', require('./routes'));
-
+  
 const db = require('./models');
 db.mongoose
   .connect(db.url, {
